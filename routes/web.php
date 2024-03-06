b8b150254651e0242048af5cfbb107ef29ff27c5
UsmanGhani <usman.ghani@totalcaresupport.com>
2024-03-06 15:26:49 +0000

    Created the User admin dashboard as well vendor admin dashboard also dynamically log in  and log out

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\UserDashboardContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\Frontend\HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -29,3 +27,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 Route::get('admin/login', [\App\Http\Controllers\Backend\AdminController::class, 'login'])->name('admin.login');
+
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.'], function (){
+    Route::get('dashboard',[UserDashboardContoller::class, 'index'])->name('dashboard');
+});
