65adec30649ab77fc95f4caccf061778fd0bbdad
UsmanGhani <usman.ghani@totalcaresupport.com>
2024-03-07 17:13:42 +0000

    Header slider feature started, created slider Model,resource controller and blades.

---
+++ resources/views/admin/slider/create.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!-- Main Content -->
+    <section class="section">
+        <div class="section-header">
+            <h1>Slider</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Create Slider</h4>
+                        </div>
+                        <div class="card-body">
+                            <form action="">
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
 /** Admin Routes */
@@ -13,3 +14,6 @@ Route::get('dashboard', [\App\Http\Controllers\Backend\AdminController::class, '
 Route::get('profile', [\App\Http\Controllers\Backend\ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [\App\Http\Controllers\Backend\ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [\App\Http\Controllers\Backend\ProfileController::class, 'updatePassword'])->name('password.update');
+
+// Slider Routes
+Route::resource('slider', SliderController::class);
