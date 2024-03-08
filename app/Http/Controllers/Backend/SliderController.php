92629ce6467b3d9d56a49ca3f41975771edbc3ca
UsmanGhani <usman.ghani@totalcaresupport.com>
2024-03-08 18:36:02 +0000

    Working on delete and update admin dashboard

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -74,7 +74,8 @@ class SliderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $slider = Slider::findOrFail($id);
+        return view('admin.slider.edit', compact('slider'));
     }
 
     /**
@@ -82,7 +83,33 @@ class SliderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'banner' => ['nullable', 'image', 'max:2000'],
+            'type' => ['string', 'max:200'],
+            'title' => ['required', 'max:200'],
+            'starting_price' => ['max:200'],
+            'btn_url' => ['url'],
+            'serial' => ['required'],
+            'status' => ['required']
+        ]);
+
+        $slider = Slider::findOrFail($id);
+
+//        handle File Upload
+        $imagePath = $this->updateImage($request, 'banner', 'uploads',  $slider->banner);
+
+        $slider->banner =  empty(!$imagePath) ? $imagePath : $slider->banner;
+        $slider->type = $request->type;
+        $slider->title = $request->title;
+        $slider->starting_price = $request->starting_price;
+        $slider->btn_url = $request->btn_url;
+        $slider->serial = $request->serial;
+        $slider->status = $request->status;
+
+        $slider->save();
+
+        toastr('UpdatedSuccessfully!', 'success');
+        return redirect()->route('admin.slider.index');
     }
 
     /**
