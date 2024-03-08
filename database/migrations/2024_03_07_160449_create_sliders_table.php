87445450af184cef9c46d910626447caff09252d
UsmanGhani <usman.ghani@totalcaresupport.com>
2024-03-08 11:06:24 +0000

    Created the Controller , migration and create view for dynamic form for Header Slider dynamic data.

--- database/migrations/2024_03_07_160449_create_sliders_table.php
+++ database/migrations/2024_03_07_160449_create_sliders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new  class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+
+            $table->text('banner')->nullable();
+            $table->string('type')->nullable();
+            $table->string('title')->nullable();
+            $table->string('starting_price')->nullable();
+            $table->string('btn_url')->nullable();
+            $table->integer('serial')->nullable();
+            $table->boolean('status')->nullable();
+
             $table->timestamps();
         });
     }
--- resources/views/admin/slider/create.blade.php
+++ resources/views/admin/slider/create.blade.php
@@ -15,7 +15,41 @@
                             <h4>Create Slider</h4>
                         </div>
                         <div class="card-body">
-                            <form action="">
+                            <form action="{{route('admin.slider.store')}}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group">
+                                    <label>Banner</label>
+                                    <input type="file" class="form-control" name="banner">
+                                </div>
+                                <div class="form-group">
+                                    <label>Type</label>
+                                    <input type="text" class="form-control" name="type" value="{{old('type')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input type="text" class="form-control" name="title" value="{{old('title')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Starting Price</label>
+                                    <input type="text" class="form-control" name="starting_price" value="{{old('starting_price')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Button URL</label>
+                                    <input type="text" class="form-control" name="btn_url" value="{{old('btn_url')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label>Serial</label>
+                                    <input type="text" class="form-control" name="serial" value="{{old('serial')}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputstate">Status</label>
+                                    <select id="inputstate" class="form-control" name="status">
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Create</button>
+
 
                             </form>
                         </div>
